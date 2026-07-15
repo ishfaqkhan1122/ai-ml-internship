6b99a95b1593e7b99e6e43d205e155df36b65878
Ishfaq Khan <ashfaqwazir44@gmail.com>
2026-07-14 23:06:11 -0700

    new folder weak-1 and Day-1

---
+++ Week 1 Python Fundamentals/Day-1/Variables-data types.py
@@ -0,0 +1,80 @@
+# Example of variable
+x = 5 
+y = "Ishfaq Khan"
+print(x) 
+print(y) 
+
+
+name=input("please enter your name=")
+age=int(input("plese enter your age="))
+print(type(name))
+print(type(age))
+
+
+#Data Types....................
+
+#---<<Basics Types>>---
+#int
+a=23  
+print(type(a))
+
+#float
+x = 3.14
+print(x)
+print(type(x))
+
+#complex
+x = 4 + 3j
+print(x)
+print(type(x))
+
+#Boolean
+x = True
+print(x)
+print(type(x))
+
+#String
+x = "This is IK"
+print(x)
+print(type(x))
+
+#<<<<<<<Container Type>>>>....
+
+#List
+Students = ["Ishfaq", "Faisal", "Hamad"]
+print(Students)
+print(type(Students))
+
+#Tuple
+numbers = (10, 20, 30)
+print(numbers)
+print(type(numbers))
+
+#Dec
+student = {"Name": "Ishfaq Khan", "Age": 20}
+print(student)
+print(type(student))
+
+#Set
+colors = {"Red", "Green", "Blue"}
+print(colors)
+print(type(colors))
+
+
+
+#Dynamic Typing....
+x = 5
+print(type(x))
+
+x = "Hello"
+print(type(x))
+
+#Multiple Variable Assignment
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+#Same Value Assignment
+x = y = z = 100
+print(x, y, z)
+
+
