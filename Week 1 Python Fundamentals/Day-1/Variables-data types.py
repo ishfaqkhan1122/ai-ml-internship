2c22a7bf3c20652926a2cef3a969dfac13675e49
Ishfaq Khan <ashfaqwazir44@gmail.com>
2026-07-15 21:56:54 -0700

    add some feaure in string

--- Week 1 Python Fundamentals/Day-1/Variables-data types.py
+++ Week 1 Python Fundamentals/Day-1/Variables-data types.py
@@ -33,12 +33,66 @@ x = True
 print(x)
 print(type(x))
 
-#String
+#------------------String----------------------------
 x = "This is IK"
 print(x)
 print(type(x))
 
-#<<<<<<<Container Type>>>>....
+#----String Operations----
+
+#<<Concatenation>>
+first_name = "Ishfaq"
+last_name = "Khan"
+print(first_name + " " + last_name)
+
+
+#<<Repetition>>
+text = "I am hassan"
+print(text * 3)
+
+#<<Find Length>>
+text = "Jamshid"
+print(len(text))
+
+#---------String Formatting--------
+#Using + add two strings
+name = "Ali"
+print("Hello " + name)
+
+#Using f-string
+name = "Sajid"
+age = 26
+print(f"My name is {name} and I am {age} years old.")
+
+#Using .format()
+name = "Wasif"
+print("Welcome {}".format(name))
+
+
+#-----String Modification--------
+#upper()
+text = "ishfaq khan is a ai student"
+print(text.upper())
+
+#lower()
+text = "ishfaq khan is a ai student"
+print(text.lower())
+
+#replace()
+text = "I like coding"
+print(text.replace("coding", "Python"))
+
+#strip()
+text = "  Ik  "
+print(text.strip())
+
+#split()
+text = "i am ishfaq from bannu"
+print(text.split())
+
+
+
+#---------------------=========<<<<<<<Container Type>>>>============....--------------------
 
 #List
 Students = ["Ishfaq", "Faisal", "Hamad"]
